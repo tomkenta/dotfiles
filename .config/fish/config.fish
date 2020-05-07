3db0c32284ece213eb0a9d8b1f30dea7af16edcd
tomkenta <tomkenta2014@gmail.com>
2020-05-07 23:35:33 +0900

    modify config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,7 +10,37 @@ alias gh='hub browse (ghq list | peco | cut -d "/" -f 2,3)'
 alias clip='pbcopy'
 alias new_note='touch (date "+%Y%m%d").md'
 
-set -g theme_powerline_fonts no
-set -g theme_nerd_fonts yes
+# PowerShell
+## font
+set -g theme_powerline_fonts yes
+
+# masterでもブランチ名を表示
+set -g theme_display_git_master_branch yes
+
+
+# 日付を表示
+set -g theme_display_date yes
+set -g theme_date_format "+%F %H:%M:%S"
+
+# コマンドの実行時間を表示
+set -g theme_display_cmd_duration yes
+
+# exitステータスを表示
+set -g theme_show_exit_status yes
+
+# shellのタイトルバー表示をカスタマイズ
+set -g theme_title_display_user no
+set -g theme_title_display_process yes
+set -g theme_title_display_path no
+
+##  postion for prompt
 set -g theme_newline_cursor yes
 set -g theme_newline_prompt '└->$ '
+
+# no abb for directory path
+set -g fish_prompt_pwd_dir_length 0
+
+# fish function (TODO)
+function cdh --description "cd and list"
+   cd $argv[1] && ls -l
+end
