73a59854d8c1d782ea28b46bd36ee2224fffb636
tomkenta <tomkenta2014@gmail.com>
2020-08-08 16:17:39 +0900

    ignore vscode extensions

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitattributes ~/.gitattributes
 ln -sf ~/dotfiles/.vscode ~/.vscode
-ln -sf ~/work/settings/dotfiles/.config/fish/config.fish ~~/.config/fish/config.fish
+ln -sf ~/work/settings/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
